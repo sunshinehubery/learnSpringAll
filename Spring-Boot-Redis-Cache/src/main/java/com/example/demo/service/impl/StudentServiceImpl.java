4af1408919284467620714919437166c6c4516eb
sunshinehubery <sunshinehubery@example.com>
2019-12-06 18:14:29 +0800

    Spring Boot中使用缓存

---
+++ Spring-Boot-Redis-Cache/src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.bean.Student;
+import com.example.demo.mapper.StudentMapper;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("studentService")
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentMapper studentMapper;
+    @Override
+    public Student update(Student student) {
+        this.studentMapper.update(student);
+        return this.queryStudentBySno(student.getSno());
+    }
+
+    @Override
+    public void deleteStudentBySno(String sno) {
+        this.studentMapper.deleteStudentBySno(sno);
+    }
+
+    @Override
+    public Student queryStudentBySno(String sno) {
+        return this.studentMapper.queryStudentBySno(sno);
+    }
+}
--- Spring-Boot-Redis-Cache/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Spring-Boot-Redis-Cache/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.bean.Student;
+import com.example.demo.service.StudentService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+    @Autowired
+    private StudentService studentService;
     @Test
     public void contextLoads() {
+        Student student1 = studentService.queryStudentBySno("001");
+        System.out.println("学号" + student1.getSno() + "的学生姓名为：" + student1.getName());
+        student1.setName("康康");
+        this.studentService.update(student1);
+        Student student2 = studentService.queryStudentBySno("001");
+        System.out.println("学号" + student2.getSno() + "的学生姓名为：" + student2.getName());
     }
 
 }
